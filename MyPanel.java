c5e464b7582f509e566f7230263e41e7891753d5
rlgh565 <rlgh565@gmail.com>
2019-12-02 13:15:28 +0900

    MyPanel 클래스 생성

---
+++ MyPanel.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.awt.*;
+/**
+ * Write a description of class MyPanel here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MyPanel extends JPanel
+{
+    public MyPanel()
+    {
+        this.setLayout(new FlowLayout());
+        this.add(new JLabel("이름"));
+        this.add(new JTextField(20));
+        this.add(new JLabel("학과"));
+        this.add(new JTextField("글소",20));
+        this.add(new JLabel("주소"));
+        this.add(new JTextField("서울..",20));
+    }
+}
