bece35d0e73bfebde24ccca08379718e382b437a
rlgh565 <rlgh565@gmail.com>
2019-12-02 14:04:14 +0900

    Label 변경

--- MyPanel.java
+++ MyPanel.java
@@ -11,16 +11,19 @@ import java.awt.event.*;
 public class MyPanel extends JPanel
 {
     private JTextArea ta = new JTextArea(7,20);
+    private JLabel mlName = new JLabel("이름");
+    private JLabel mlDept = new JLabel("학과");
+    private JLabel mlAddress = new JLabel("");
     private JButton btn1 = new JButton("추가");
     private JButton btn2 = new JButton("Clear");
     public MyPanel()
     {
         this.setLayout(new FlowLayout());
-        this.add(new JLabel("이름"));
+        this.add(mlName);
         this.add(new JTextField(20));
-        this.add(new JLabel("학과"));
+        this.add(mlDept);
         this.add(new JTextField("글소",20));
-        this.add(new JLabel("주소"));
+        this.add(mlAddress);
         this.add(new JTextField("서울..",20));
         this.add(new JLabel("학년"));
         JComboBox jcb = new JComboBox(new String[] {"1학년","2학년","3학년","4학년"});
